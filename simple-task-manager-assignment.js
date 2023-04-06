dc33f1a0d50b876169de80c6f22264596b59effd
Kevin Hoang <kevin.hoang199@gmail.com>
2023-04-06 13:52:10 -0400

    (hopefully) met all requirements

---
+++ simple-task-manager-assignment.js
@@ -0,0 +1,24 @@
+let taskmanager = (function () {
+  let tasks = [];
+
+  function addTask(string) {
+    tasks.push(string);
+  }
+
+  function displayTasks() {
+    for (let task of tasks) {
+      console.log(task);
+    }
+  }
+
+  return {
+    addTask: addTask,
+    displayTasks: displayTasks,
+  };
+})();
+
+taskmanager.addTask("Schedule appointment for dog groomer."); // Task 1
+taskmanager.addTask("Clean gutters"); // Task 2
+taskmanager.addTask("Call my mom"); // Task 3
+
+taskmanager.displayTasks(); // Printing tasks using method in function
